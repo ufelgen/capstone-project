06c4eaec94c29f0717744f853276ecd9f3234c85
Ulrike Felgenhauer <115288914+ufelgen@users.noreply.github.com>
2022-12-13 14:31:15 +0100

    Feature/us1 basic vocabulary (#6)
    * render dummy data on single page
    
    * adapt styling for vocab cards
    
    * remove unnecessary lines of code
    
    * add suggestions by reviewers and install nanoid

---
+++ components/CardSection/CategoryAnimals.js
@@ -0,0 +1,55 @@
+import { words } from "../../dummydata/words";
+import { Fragment } from "react";
+import styled from "styled-components";
+
+export default function CardSection() {
+  const allCategories = words.map((word) => word.category);
+  const singleCategories = Array.from(new Set(allCategories));
+  const animals = words.filter((word) => word.category === "animals");
+
+  return (
+    <Fragment>
+      <StyledHeader>{singleCategories[0]}</StyledHeader>
+      <CardWrapper>
+        {animals.map((animal) => (
+          <Fragment key={animal.id}>
+            <StyledCard>
+              <p>{animal.base.translation}</p>
+              <p>{animal.query1.translation}</p>
+            </StyledCard>
+          </Fragment>
+        ))}
+      </CardWrapper>
+    </Fragment>
+  );
+}
+
+const CardWrapper = styled.article`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  background-color: white;
+`;
+
+const StyledCard = styled.div`
+  padding: 10px;
+  margin: 10px 12px;
+  background-color: white;
+  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+  color: black;
+  text-decoration: none;
+  list-style: none;
+  height: auto;
+  border: 1px solid darkmagenta;
+  box-shadow: 4px 4px 4px 0.7px rgba(130, 8, 130, 0.43);
+  p {
+    padding: 7px 0px;
+  }
+`;
+
+const StyledHeader = styled.h2`
+  margin: 10px;
+  background-color: White;
+  color: darkmagenta;
+  text-align: center;
+  padding: 7px;
+`;
--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -3,13 +3,13 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
     html,
     body {
-        padding: 0;
-        margin: 0;
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     }
 
     * {
         box-sizing: border-box;
+        margin: 0;
+        padding: 0;
     }
 `;
 
