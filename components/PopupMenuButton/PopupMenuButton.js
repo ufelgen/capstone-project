062cc87c8ced23b01e9b3513823745840577e044
Ulrike Felgenhauer <115288914+ufelgen@users.noreply.github.com>
2022-12-19 15:43:21 +0100

    add a popup menu with a delete word function (#15)
    * add a popup menu awith a delete word function
    
    * remove unnecessary fragments
    
    * fix form overflow

---
+++ components/PopupMenuButton/PopupMenuButton.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { HiDotsVertical } from "react-icons/hi";
+
+export default function PopupMenuButton({ id, onPopupClick }) {
+  return (
+    <StyledPopupMenuButton onClick={() => onPopupClick(id)}>
+      <HiDotsVertical size={15} />
+    </StyledPopupMenuButton>
+  );
+}
+
+const StyledPopupMenuButton = styled.button`
+  position: absolute;
+  right: 10%;
+  bottom: 10%;
+  border: none;
+`;
