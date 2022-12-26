a5d68fd90959d38194005eed56fb6b915441c434
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2022-12-26 22:17:23 +0100

    fix declension/index.js

---
+++ pages/declension/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function DeclensionHome() {
+  return (
+    <>
+      <h2>there's nothing here</h2>
+      <Link href={"/"}>back to main page</Link>
+    </>
+  );
+}
