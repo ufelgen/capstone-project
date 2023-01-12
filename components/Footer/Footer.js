473fbdc70927da5c4bc2bc5fd396071e6a6be208
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2023-01-12 15:03:30 +0100

    delete unused import

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Router from "next/router";
