c15389b34e02cfda38f240781a2355b3a7fa3766
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2023-01-15 13:21:07 +0100

    configure next-auth step 1

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const providers = [];
+if (process.env.VERCEL_ENV === "preview") {
+  providers.push(
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === "extreme-beaver" &&
+          credentials.password === "extreme-beaver"
+        ) {
+          return {
+            name: "Beaver Test User",
+            email: "test@example.com",
+          };
+        } else {
+          return null;
+        }
+      },
+    })
+  );
+} else {
+  providers.push(
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    })
+  );
+}
+
+export const authOptions = {
+  providers,
+};
+export default NextAuth(authOptions);
