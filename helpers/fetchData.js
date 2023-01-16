dac97b975f202f4b3f8ee2eaf38e3d66fdc55f09
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2023-01-16 15:14:47 +0100

    -update fetch function to return empty array if no words

--- helpers/fetchData.js
+++ helpers/fetchData.js
@@ -1,9 +1,13 @@
 export default async function fetchData() {
   try {
     const response = await fetch("/api/words");
-    const data = await response.json();
-    console.log(data);
-    return data;
+    if (response.status === 200) {
+      const data = await response.json();
+      return data;
+    } else {
+      const data = [];
+      return data;
+    }
   } catch (error) {
     console.error(error);
   }
