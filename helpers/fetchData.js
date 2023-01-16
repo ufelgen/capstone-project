f8dee3f10cfe690970f480d01b92e75d45fd345b
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2023-01-16 15:10:28 +0100

    only render object associated to user email

--- helpers/fetchData.js
+++ helpers/fetchData.js
@@ -2,6 +2,7 @@ export default async function fetchData() {
   try {
     const response = await fetch("/api/words");
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
