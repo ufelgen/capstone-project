8ce3f996ed1095f777ed2bee5324defab0230316
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2023-01-18 11:15:17 +0100

    conjugation index.js

--- pages/conjugation/index.js
+++ pages/conjugation/index.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import styled from "styled-components";
 import { StyledHeading } from "../[category]";
+import Lottie from "lottie-react";
+import astronautMusic from "../../public/Lottie/astronautMusic.json";
 
 export default function ConjugationHome() {
   return (
     <>
       <StyledHeading>there's nothing here</StyledHeading>
       <Main>
-        <Link href={"/"}>
+        <Lottie animationData={astronautMusic} loop={true} />
+        <StyledLink href={"/"}>
           <p>back to main page</p>
-        </Link>
+        </StyledLink>
       </Main>
     </>
   );
@@ -17,6 +20,14 @@ export default function ConjugationHome() {
 
 const Main = styled.section`
   display: flex;
-  justify-content: center;
-  align-self: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  background-color: darkmagenta;
+  color: white;
+  padding: 0.5rem;
+  border-radius: 5px;
 `;
